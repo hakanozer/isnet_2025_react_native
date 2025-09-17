876a64231feb7b8657e5cb405041a07a4225878b
Hakan ÖZER <hakanozer02@gmail.com>
2025-09-17 13:04:44 +0300

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,44 +1,48 @@
-import React from 'react';
-import { StatusBar, StyleSheet, Text, View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-
-export default function IndexScreen() {
+export default function Index() {
   return (
     <SafeAreaView style={styles.safeArea}>
-      <StatusBar barStyle="dark-content" />
-      <View style={styles.container}>
-        <Text style={styles.title}>ISNET 2025</Text>
-        <Text style={styles.subtitle}>Tek ekranlık minimal örnek</Text>
-      </View>
+        <ScrollView contentContainerStyle={styles.scrollView} keyboardShouldPersistTaps="handled">
+           <View style={styles.container}>
+          <Text style={styles.title}>User Login</Text>
+          <TextInput placeholder="E-Mail" autoCapitalize="none" keyboardType="email-address" style={styles.input} />
+          <TextInput placeholder="Password" autoCapitalize="none" secureTextEntry style={styles.input} />
+        </View>
+        </ScrollView>
     </SafeAreaView>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
-    backgroundColor: '#ffffff',
+    backgroundColor: '#ffffff'
+  },
+  scrollView: {
+    flexGrow: 1,
   },
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    paddingHorizontal: 24,
-    backgroundColor: '#ffffff',
+    paddingHorizontal: 10,
   },
   title: {
-    fontSize: 28,
-    fontWeight: '700',
-    color: '#111827',
-    marginBottom: 8,
+    fontSize: 23,
+    fontWeight: '600',
+    color: '#333333'
   },
-  subtitle: {
+  input: {
+    marginTop: 20,
+    width: '100%',
+    height: 40,
+    borderColor: '#cccccc',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10,
     fontSize: 16,
-    color: '#374151',
-  },
-});
-
-
-
-
+    color: '#000000'
+  }
+})
